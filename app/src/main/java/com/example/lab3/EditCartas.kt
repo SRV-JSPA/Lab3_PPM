54b20a23f2a8a59f0e7e0a1f167915ec215e7858
JSPA <per22318@uvg.edu.gt>
2023-08-31 23:33:06 -0600

    escribir en imagenes

--- app/src/main/java/com/example/lab3/EditCartas.kt
+++ app/src/main/java/com/example/lab3/EditCartas.kt
@@ -3,13 +3,32 @@ package com.example.lab3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab3.ui.theme.Lab3Theme
 
 class EditCartas : ComponentActivity() {
@@ -26,6 +45,43 @@ class EditCartas : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun eImagen(imageResId: Int) {
+    val contexto = LocalContext.current
+
+    var texto by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        BasicTextField(
+            value = texto,
+            onValueChange = { newText -> texto = newText },
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
     }
 }
 
